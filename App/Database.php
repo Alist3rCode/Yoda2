a586c52be388f2d3e2d95d990326bd6675ce90f6
Alist3rW <lopez.yohann@gmail.com>
2018-08-12 10:26:22 +0200

    Update Database.php

--- App/Database.php
+++ App/Database.php
@@ -20,13 +20,10 @@ class Database{
         $pdo = new PDO('mysql:dbname=yoda;host=localhost','root');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $this->pdo = $pdo;
-        // die(var_dump($this->pdo));
     }
 
     public function query($statement){
-        // die(var_dump($this->getPDO()));
         $res = $this->getPDO()->query($statement);
-        // var_dump($res);
         $datas = $res->fetchAll(PDO::FETCH_OBJ);
         return $datas;
 
