a98ec18bde5f47f5793b54bb90a6daf0de3af539
Alist3rW <lopez.yohann@gmail.com>
2018-08-28 10:19:26 +0200

    correction for customer with no secondary sites

--- ajax/getModif.php
+++ ajax/getModif.php
@@ -6,9 +6,10 @@ require "../class/convertJson.php";
 
 $client = $bdd->query('SELECT * '
         . 'FROM YDA_CLIENT '
-        . 'INNER JOIN YDA_PHONE ON CLI_ID = PHO_ID_CLI AND PHO_VALID = 1 '
-        . 'INNER JOIN YDA_MAPS ON MPS_ID_PHO = PHO_ID '
+        . 'LEFT JOIN YDA_PHONE ON CLI_ID = PHO_ID_CLI AND PHO_VALID = 1 '
+        . 'LEFT JOIN YDA_MAPS ON MPS_ID_PHO = PHO_ID '
         . 'WHERE CLI_ID="' . $_REQUEST['id'] . '"', 'Clients');
 
 header("content-type:application/json");
+
 echo json_encode(convert_json($client));
