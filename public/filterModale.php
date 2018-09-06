364bcdeaca94e1529c9595431d6ac6524c72ba14
Alist3rW <lopez.yohann@gmail.com>
2018-09-06 18:05:12 +0200

    Advanced filters

--- public/filterModale.php
+++ public/filterModale.php
@@ -1,13 +1,18 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Filtres Avancés</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        ...
+      <div class="modal-body row">
+          <div class="col-4" style="background:blue">
+              <p>plop </p>
+          </div>
+          <div class="col-8" style="background:red">
+              <p>plop </p>
+          </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
